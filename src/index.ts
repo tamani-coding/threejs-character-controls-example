0de6a790b74a9a5c7cbaec46d4fe9e2892fca30f
genka <tamani.coding@gmail.com>
2021-09-18 10:25:59 +0200

    scene setup

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 25;
+camera.position.z = 50;
+camera.position.x = -50;
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true
+controls.target = new THREE.Vector3(0, 0, -25);
+controls.update();
+
+// AMBIENT LIGHT
+scene.add(new THREE.AmbientLight(0xffffff, 1));
+
+// TEXTURES
+const textureLoader = new THREE.TextureLoader();
+
+const sandBaseColor = textureLoader.load("./textures/sand/Sand 002_COLOR.jpg");
+const sandNormalMap = textureLoader.load("./textures/sand/Sand 002_NRM.jpg");
+const sandHeightMap = textureLoader.load("./textures/sand/Sand 002_DISP.jpg");
+const sandAmbientOcclusion = textureLoader.load("./textures/sand/Sand 002_OCC.jpg");
+
+// FLOOR
+const WIDTH = 50
+const LENGTH = 50
+const NUM_X = 6
+const NUM_Z = 6
+for (let i = 0; i < NUM_X; i++) {
+    for (let j = 0; j < NUM_Z; j++) {
+        const floor = new THREE.Mesh(new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512), new THREE.MeshStandardMaterial(
+            {
+                map: sandBaseColor, normalMap: sandNormalMap,
+                displacementMap: sandHeightMap, displacementScale: 0.08,
+                aoMap: sandAmbientOcclusion
+            }))
+        floor.receiveShadow = true
+        floor.rotation.x = - Math.PI / 2
+
+        floor.position.x = i * WIDTH - (NUM_X / 2) * WIDTH
+        floor.position.z = j * LENGTH - (NUM_Z / 2) * LENGTH
+
+        scene.add(floor)
+    }
+}
+
+
+// ANIMATE
+function animate() {
+    controls.update()
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+document.body.appendChild(renderer.domElement);
+animate();
+
+// RESIZE HANDLER
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'src');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: src
+  },
+  devServer: {
+    contentBase: src,
+  },
+}
