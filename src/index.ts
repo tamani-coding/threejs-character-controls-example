ddd0bf29ccbbc577e7a30cc3ff4ed67361e2bb8a
genka <tamani.coding@gmail.com>
2021-09-18 10:28:24 +0200

    refactore

--- src/index.ts
+++ src/index.ts
@@ -26,36 +26,8 @@ controls.update();
 // AMBIENT LIGHT
 scene.add(new THREE.AmbientLight(0xffffff, 1));
 
-// TEXTURES
-const textureLoader = new THREE.TextureLoader();
-
-const sandBaseColor = textureLoader.load("./textures/sand/Sand 002_COLOR.jpg");
-const sandNormalMap = textureLoader.load("./textures/sand/Sand 002_NRM.jpg");
-const sandHeightMap = textureLoader.load("./textures/sand/Sand 002_DISP.jpg");
-const sandAmbientOcclusion = textureLoader.load("./textures/sand/Sand 002_OCC.jpg");
-
 // FLOOR
-const WIDTH = 50
-const LENGTH = 50
-const NUM_X = 6
-const NUM_Z = 6
-for (let i = 0; i < NUM_X; i++) {
-    for (let j = 0; j < NUM_Z; j++) {
-        const floor = new THREE.Mesh(new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512), new THREE.MeshStandardMaterial(
-            {
-                map: sandBaseColor, normalMap: sandNormalMap,
-                displacementMap: sandHeightMap, displacementScale: 0.08,
-                aoMap: sandAmbientOcclusion
-            }))
-        floor.receiveShadow = true
-        floor.rotation.x = - Math.PI / 2
-
-        floor.position.x = i * WIDTH - (NUM_X / 2) * WIDTH
-        floor.position.z = j * LENGTH - (NUM_Z / 2) * LENGTH
-
-        scene.add(floor)
-    }
-}
+generateFloor()
 
 
 // ANIMATE
@@ -74,3 +46,35 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 window.addEventListener('resize', onWindowResize);
+
+function generateFloor() {
+    // TEXTURES
+    const textureLoader = new THREE.TextureLoader();
+
+    const sandBaseColor = textureLoader.load("./textures/sand/Sand 002_COLOR.jpg");
+    const sandNormalMap = textureLoader.load("./textures/sand/Sand 002_NRM.jpg");
+    const sandHeightMap = textureLoader.load("./textures/sand/Sand 002_DISP.jpg");
+    const sandAmbientOcclusion = textureLoader.load("./textures/sand/Sand 002_OCC.jpg");
+
+    const WIDTH = 50
+    const LENGTH = 50
+    const NUM_X = 6
+    const NUM_Z = 6
+    for (let i = 0; i < NUM_X; i++) {
+        for (let j = 0; j < NUM_Z; j++) {
+            const floor = new THREE.Mesh(new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512), new THREE.MeshStandardMaterial(
+                {
+                    map: sandBaseColor, normalMap: sandNormalMap,
+                    displacementMap: sandHeightMap, displacementScale: 1,
+                    aoMap: sandAmbientOcclusion
+                }))
+            floor.receiveShadow = true
+            floor.rotation.x = - Math.PI / 2
+
+            floor.position.x = i * WIDTH - (NUM_X / 2) * WIDTH
+            floor.position.z = j * LENGTH - (NUM_Z / 2) * LENGTH
+
+            scene.add(floor)
+        }
+    }
+}
